2e8da7ec73c5d11e584fda0c69b68f5e963898c2
SAKTHIKESAVAN <sakthik702101997@gmail.com>
2017-06-07 09:31:55 +0530

    Create number1.java

---
+++ number1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class number1
+{
+public static void main(String a[])
+{int i=0;
+Scanner sc=new Scanner(System.in);
+int b=sc.nextInt();
+if(b>0){
+System.out.print("positive");
+}
+if(b<0)
+{System.out.print("negative");
+}
+if(b==0)
+{System.out.print("zero");
+}
+}
+}
